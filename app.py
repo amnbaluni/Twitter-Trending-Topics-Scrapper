76fd9743b3f0b86259b96c800df767aae844f8ae
Aman Baluni <amanbaluni.abab@gmail.com>
2024-12-24 20:24:46 +0530

    added comments in app.py

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
+# Import necessary modules from Flask and MongoDB libraries
 from flask import Flask, render_template
 from pymongo import MongoClient
 import subprocess
 from bson.json_util import dumps
 from bson import ObjectId
 
+# Create a Flask web application
 app = Flask(__name__)
+
+# Connect to the MongoDB database running on localhost at port 27017
 client = MongoClient('mongodb://localhost:27017/')
+
+# Select the 'twitter_trends' database
 db = client['twitter_trends']
+
+# Select the 'trends' collection within the 'twitter_trends' database
 collection = db['trends']
 
+# Define a route for the root URL ("/")
 @app.route('/')
 def index():
+    """
+    This function renders the index.html template.
+
+    Parameters:
+    None
+
+    Returns:
+    render_template('index.html'): The rendered index.html template.
+    """
     return render_template('index.html')
 
+# Define a route for the "/run-script" URL
 @app.route('/run-script')
 def run_script():
     
@@ -27,5 +46,6 @@ def run_script():
     
     return render_template('result.html', record=dumps(latest_record))
 
+# Run the Flask web application in debug mode
 if __name__ == '__main__':
     app.run(debug=True)
